e1140a1d96a52377e8aa3ad2fc1fe96c676896df
Johann Mayos(요한) <42842871+johannmay@users.noreply.github.com>
2022-06-02 01:37:48 -0400

    Lvl 1 scenario and player added

---
+++ Pandemic Biohazard/Assets/Laboratory/lab tileset finished transparent.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="lab tileset finished transparent" tilewidth="8" tileheight="8" tilecount="8640" columns="120">
+ <image source="lab tileset finished transparent.png" width="960" height="576"/>
+</tileset>
